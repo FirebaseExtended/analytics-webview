1a70587b5d1715e90b7250d43fb30a18ab0d48e3
Morgan Chen <morganchen12@gmail.com>
2016-09-29 17:04:36 -0700

    add swift sample

---
+++ ios/swift/FirebaseAnalyticsWeb/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  Copyright (c) 2016 Google Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import WebKit
+
+import FirebaseAnalytics
+
+class ViewController: UIViewController, WKScriptMessageHandler {
+
+  private var webView: WKWebView!
+  private var projectURL: URL!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Get the hosted site url from the GoogleService-Info.plist file.
+    let plistPath = Bundle.main.path(forResource: "GoogleService-Info",
+                                     ofType: "plist")!
+    let plist = NSDictionary(contentsOfFile: plistPath)!
+    let appID = plist["PROJECT_ID"] as! String
+
+    let projectURLString = "https://\(appID).firebaseapp.com"
+    self.projectURL = URL(string: projectURLString)!
+
+    // Initialize the webview and add self as a script message handler.
+    let config = WKWebViewConfiguration()
+    config.userContentController.add(self, name: "firebase")
+
+    self.webView = WKWebView(frame: self.view.frame, configuration: config)
+    self.view.addSubview(self.webView)
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+
+    let request = URLRequest(url: self.projectURL)
+    self.webView.load(request)
+  }
+
+  func userContentController(_ userContentController: WKUserContentController,
+                             didReceive message: WKScriptMessage) {
+    guard let body = message.body as? [String: Any] else { return }
+    guard let command = body["command"] as? String else { return }
+    guard let name = body["name"] as? String else { return }
+
+    if command == "setUserProperty" {
+      guard let value = body["value"] as? String else { return }
+      FIRAnalytics.setUserPropertyString(value, forName: name)
+    } else if command == "logEvent" {
+      guard let params = body["parameters"] as? [String: NSObject] else { return }
+      FIRAnalytics.logEvent(withName: name, parameters: params)
+    }
+  }
+
+}
+
